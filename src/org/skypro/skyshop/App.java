1ac27a288f55428ea7a05fadee56fa08ac4c39d9
ranis <ranis.rafisovich@bk.ru>
2025-10-03 19:53:01 +0300

    12

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -16,14 +16,13 @@ public class App {
         ProductBasket basket = new ProductBasket();
 
 
-
         SimpleProduct apple1 = new SimpleProduct("Яблоко", -50.0);
         SimpleProduct banana = new SimpleProduct("Банан", 70.0);
-        FixPriceProduct apple2 = new FixPriceProduct("Яблоко") ;
+        FixPriceProduct apple2 = new FixPriceProduct("Яблоко");
         DiscountedProduct orange = new DiscountedProduct("Апельсин", 60.0, 120);
         SimpleProduct apple3 = new SimpleProduct("Яблоко", 52.0);
 
-        Article Article1 = new Article("Бананы из юга","Какой то текст");
+        Article Article1 = new Article("Бананы из юга", "Какой то текст");
         Article test = new Article("Русские яблочки", "Текст другого содержания");
 
         basket.addProduct(apple1);
@@ -52,15 +51,15 @@ public class App {
 
     }
 
-    public static void multiCatch(){
+    public static void multiCatch() {
         try {
             SimpleProduct apple1 = new SimpleProduct("Яблоко", -50.0);
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println("Цена указана неверно");
         }
         try {
             DiscountedProduct orange = new DiscountedProduct("Апельсин", 60.0, 120);
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println("Скидка указана неверно");
         }
     }
